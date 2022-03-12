0e43c9c636924218d6063718e6adce343df94857
edu8169 <62029234+edu8169@users.noreply.github.com>
2022-03-11 20:34:16 -0600

    Game routes added

--- server/src/modules/Game/game.routes.js
+++ server/src/modules/Game/game.routes.js
@@ -1,5 +1,25 @@
 const router = require('express').Router();
 
+//CREATE
+router.post('/', (req, res)=>{
+    res.send('Game created')    
+});
+//DELETE
+router.delete('/:id', (req, res)=>{
+    const id = req.params.id;
+    res.send('Game ' + id + ' deleted.');
+});
+//GETINFO
+router.get('/:id', (req, res)=>{
+    const id = req.params.id;
+    res.send('Game ' + id);
+});
+//MODIFY
+router.put('/:id', (req, res)=>{
+    const id = req.params.id;
+    res.send(id + 'Game modified');
+});
+
 
 
 
