41e159017b07436b4f72815c227e94e85b863c3d
Rodrigo <is719485@iteso.mx>
2022-03-11 20:04:54 -0600

    channel routing done

--- server/src/modules/Channel/channel.routes.js
+++ server/src/modules/Channel/channel.routes.js
@@ -1,6 +1,23 @@
 const router = require('express').Router();
 
-
-
+//CREATE
+router.post('/', (req, res)=>{
+    res.send('Channel created')    
+});
+//DELETE
+router.delete('/:id', (req, res)=>{
+    const id = req.params.id;
+    res.send('Channel ' + id + ' deleted.');
+});
+//GETINFO
+router.get('/:id', (req, res)=>{
+    const id = req.params.id;
+    res.send('Channel ' + id);
+});
+//MODIFY
+router.put('/:id', (req, res)=>{
+    const id = req.params.id;
+    res.send(id + 'Channel modified');
+});
 
 module.exports = router;
