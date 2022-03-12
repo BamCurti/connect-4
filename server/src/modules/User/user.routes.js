e195191e9c46985147c0cf4687618a33d4d86ea0
Rodrigo <is719485@iteso.mx>
2022-03-11 20:36:16 -0600

    user-routing created

--- server/src/modules/User/user.routes.js
+++ server/src/modules/User/user.routes.js
@@ -1,6 +1,24 @@
 const router = require('express').Router();
 
-
+//CREATE
+router.post('/', (req, res)=>{
+    res.send('User created.')    
+});
+//DELETE
+router.delete('/:id', (req, res)=>{
+    const id = req.params.id;
+    res.send('User ' + id + ' deleted.');
+});
+//GETINFO
+router.get('/:id', (req, res)=>{
+    const id = req.params.id;
+    res.send('User ' + id);
+});
+//MODIFY
+router.put('/:id', (req, res)=>{
+    const id = req.params.id;
+    res.send(id + ' user modified.');
+});
 
 
 module.exports = router;
