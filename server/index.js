d0ba506e61ce267ed2acc0085d62d72ced3ef544
Edgar Vallejo Curti <bam_vllj98@outlook.com>
2022-03-21 14:18:15 -0600

    signup validation complete

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 //Dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 //Import routes
 const router = require('./src/routes');
+const { boomError, internalError } = require('./src/core/middlewares/error.handler');
 
 //Configure enviorement variables 
 require("dotenv").config();
@@ -11,8 +14,18 @@ const port = process.env.PORT || 3000;
 //Init app
 const app = express();
 
+//set body parser
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 //Set Api endpoint
 app.use('/api', router);
+
+//set middlewares
+app.use(boomError);
+app.use(internalError);
 
 //List to port
 app.listen(port, () =>{
