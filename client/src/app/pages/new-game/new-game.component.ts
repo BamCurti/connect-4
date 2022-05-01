943b0aaca15ea65ae467d26c80e68e36b3f0a65d
Edgar Vallejo Curti <bam_vllj98@outlook.com>
2022-05-01 16:13:03 -0500

    form read external key

--- client/src/app/pages/new-game/new-game.component.ts
+++ client/src/app/pages/new-game/new-game.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-game',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-game.component.scss']
 })
 export class NewGameComponent implements OnInit {
+  url = 'http://localhost:3000/api/game';
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    formBuilder: FormBuilder
+  ) {
+    this.form = formBuilder.group({
+      'key': ['', Validators.required]
+    })
+   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onSubmit() {
+    if(!this.form.valid) {
+      alert('Please enter a valid key')
+      return;
+    }
+
+    const form = this.form.getRawValue();
+    console.log(`${this.url}/${form.key}`);
+
   }
 
 }
