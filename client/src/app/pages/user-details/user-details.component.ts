6363d2e549e72cd5833129b939e8908e1c25b0d6
edu8169 <62029234+edu8169@users.noreply.github.com>
2022-05-02 21:23:41 -0500

    Added user-detail component

--- client/src/app/pages/user-details/user-details.component.ts
+++ client/src/app/pages/user-details/user-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { LoginService } from 'src/app/shared/services/login.service';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/shared/services/user.service'
+
 
 @Component({
   selector: 'app-user-details',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+  credentials: any = {};
+  id : string = '';
+  user: Object = {};
+
+  constructor(
+    private loginService: LoginService,
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    })
+    this.userService.getUser(this.id).then(response => {
+      this.user = response;
+    }).catch(error =>{
+      console.log('No se presentaron datos')
+    }).finally(() =>{
+
+    })
+  }
+
+  login(){
+    console.log('Enviar Datos', this.credentials);
   }
 
 }
--- client/src/app/shared/services/user.service.ts
+++ client/src/app/shared/services/user.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { environment } from '../../../environments/environment'
+import axios from 'axios';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  url = `${environment.apiUrl}/user`
   constructor() { }
+
+  getUsers() {
+    return axios.get(this.url);
+  }
+  getUser(id: string) {
+    return axios.get(`${this.url}/${id}`)
+  }
+
 }
